d1ab1ee36c046181e4ca5b7eb29e6c40fbf77458
Sjoerd van der Bok <sabok@avans.nl>
2018-11-27 22:56:18 +0100

    test commit

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+const UserController = require('../controllers/user_controller');
+
+module.exports = (app) => {
+
+    /*
+    We moeten nog even zorgen dat dit werkt zodat de functionaliteit van de requests
+    naar de user_controller wordt verplaatst en het niet hier staat.
+    app.post('/api/users', UsersController.create);
+    app.put('/api/users/:id', UsersController.edit);
+    app.delete('/api/users/:id', UsersController.delete);
+    */
+
+}
