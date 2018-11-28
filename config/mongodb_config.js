cccec138347a2effe256209f08e9798510bca8fd
Sjoerd van der Bok <sabok@avans.nl>
2018-11-28 17:02:44 +0100

    Added Heroku support for MLabs

---
+++ config/mongodb_config.js
@@ -0,0 +1,17 @@
+var env = {
+    webPort: process.env.PORT || 3000,
+    dbHost: process.env.DB_HOST || 'localhost',
+    dbPort: process.env.DB_PORT || 'ds139841',
+    dbUser: process.env.DB_USER || 'sabok',
+    dbPassword: process.env.DB_PASSWORD || 'potato1',
+    dbDatabase: process.env.DB_DATABASE || 'studdit_db'
+}
+
+var dburl = process.env.NODE_ENV === 'production' ?
+    'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase :
+    'mongodb://localhost/' + env.dbDatabase
+
+module.exports = {
+    env: env,
+    dburl: dburl
+};
--- config/mongodb_connector.js
+++ config/mongodb_connector.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
+const config = require('./mongodb_config');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://sabok:potato1@ds139841.mlab.com:39841/studdit_db', { useNewUrlParser: true });
-var connection = mongoose.connection
-    .once('open', () => console.log('Connected to MLabs'))
+mongoose.connect(config.dburl);
+    var connection = mongoose.connection
+    .once('open', () => console.log('Connected to Mongo on ' + config.dburl))
     .on('error', (error) => {
         console.warn('Warning', error.toString());
     });
+
+//mongoose.connect('mongodb://sabok:potato1@ds139841.mlab.com:39841/studdit_db', { useNewUrlParser: true });
+//  var connection = mongoose.connection
+//  .once('open', () => console.log('Connected to MLabs'))
+//  .on('error', (error) => {
+//      console.warn('Warning', error.toString());
+//  });
 
 module.exports = connection;
