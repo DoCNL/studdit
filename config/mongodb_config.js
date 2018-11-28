6e2aaa10ed5d5fee82127ea87cc83592722d6cd2
Sjoerd van der Bok <sabok@avans.nl>
2018-11-28 17:14:42 +0100

    Heroku bugfix

--- config/mongodb_config.js
+++ config/mongodb_config.js
@@ -1,7 +1,7 @@
 var env = {
     webPort: process.env.PORT || 3000,
-    dbHost: process.env.DB_HOST || 'localhost',
-    dbPort: process.env.DB_PORT || 'ds139841',
+    dbHost: process.env.DB_HOST || 'ds139841.mlab.com',
+    dbPort: process.env.DB_PORT || '39841',
     dbUser: process.env.DB_USER || 'sabok',
     dbPassword: process.env.DB_PASSWORD || 'potato1',
     dbDatabase: process.env.DB_DATABASE || 'studdit_db'
