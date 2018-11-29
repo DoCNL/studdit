ef6c08aa0934c06bd658109c9116166c6708b9ea
Sjoerd van der Bok <sabok@avans.nl>
2018-11-29 12:19:46 +0100

    fix

--- routes/routes.js
+++ routes/routes.js
@@ -3,16 +3,16 @@ const ThreadController = require('../controllers/thread_controller');
 
 module.exports = (app) => {
 
-    //create user
+    //create a new user
     app.post('/api/users/add', UserController.create);
-    //change password
+    //change password of an existing user
     app.put('/api/users/edit', UserController.edit);
     //delete user
     app.delete('/api/users/delete', UserController.delete);
 
-    //create thread
+    //create a new thread
     app.post('/api/thread/add',ThreadController.create);
-    //edit
+    //edit an existing thread
     app.post('/api/thread/edit',ThreadController.edit);
 
 };
