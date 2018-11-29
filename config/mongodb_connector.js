2125ad8b60a4085831038f0fce0e224677d868e2
Sjoerd van der Bok <sabok@avans.nl>
2018-11-29 11:10:07 +0100

    Heroku bugfix

--- config/mongodb_connector.js
+++ config/mongodb_connector.js
@@ -3,7 +3,8 @@ const config = require('./mongodb_config');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(config.dburl);
+//mongoose.connect(config.dburl);
+mongoose.connect('mongodb://sabok:potato1@ds139841.mlab.com:39841/studdit_db', { useNewUrlParser: true });
     var connection = mongoose.connection
     .once('open', () => console.log('Connected to Mongo on ' + config.dburl))
     .on('error', (error) => {
